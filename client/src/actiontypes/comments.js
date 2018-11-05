42f03f5dc71ae4b57204a10cade621bd3b9e38d8
Alexander Droste <alexander.droste@progmem.de>
2018-11-05 13:31:05 +0100

    client: fix some actiontype definitions

--- client/src/actiontypes/comments.js
+++ client/src/actiontypes/comments.js
@@ -1,24 +1,24 @@
 
 /** comments actiontype: change a users vote */
-export const CHANGE_VOTE_REQUEST = 'events/CHANGE_VOTE_REQUEST';
+export const CHANGE_VOTE_REQUEST = 'comments/CHANGE_VOTE_REQUEST';
 /** comments actiontype: change a users vote */
-export const CHANGE_VOTE_SUCCESS = 'events/CHANGE_VOTE_SUCCESS';
+export const CHANGE_VOTE_SUCCESS = 'comments/CHANGE_VOTE_SUCCESS';
 /** comments actiontype: change a users vote */
-export const CHANGE_VOTE_FAILURE = 'events/CHANGE_VOTE_FAILURE';
+export const CHANGE_VOTE_FAILURE = 'comments/CHANGE_VOTE_FAILURE';
 
 /** comments actiontype: delete a comment */
-export const DELETE_COMMENT_REQUEST = 'events/DELETE_COMMENT_REQUEST';
+export const DELETE_COMMENT_REQUEST = 'comments/DELETE_COMMENT_REQUEST';
 /** comments actiontype: delete a comment */
-export const DELETE_COMMENT_SUCCESS = 'events/DELETE_COMMENT_SUCCESS';
+export const DELETE_COMMENT_SUCCESS = 'comments/DELETE_COMMENT_SUCCESS';
 /** comments actiontype: delete a comment */
-export const DELETE_COMMENT_FAILURE = 'events/DELETE_COMMENT_FAILURE';
+export const DELETE_COMMENT_FAILURE = 'comments/DELETE_COMMENT_FAILURE';
 
 /** comments actiontype: post a comment */
-export const POST_COMMENT_REQUEST = 'events/POST_COMMENT_REQUEST';
+export const POST_COMMENT_REQUEST = 'comments/POST_COMMENT_REQUEST';
 /** comments actiontype: post a comment */
-export const POST_COMMENT_SUCCESS = 'events/POST_COMMENT_SUCCESS';
+export const POST_COMMENT_SUCCESS = 'comments/POST_COMMENT_SUCCESS';
 /** comments actiontype: post a comment */
-export const POST_COMMENT_FAILURE = 'events/POST_COMMENT_FAILURE';
+export const POST_COMMENT_FAILURE = 'comments/POST_COMMENT_FAILURE';
 
 /** comments actiontype: subscribe to comments */
 export const SUBSCRIBE_COMMENTS_FOR_ENTRY_REQUEST = 'comments/SUBSCRIBE_COMMENTS_FOR_ENTRY_REQUEST';
--- client/src/actiontypes/entries.js
+++ client/src/actiontypes/entries.js
@@ -1,72 +1,79 @@
 /** entries actiontype: change bookmarked state of entry */
-export const CHANGE_BOOKMARK_REQUEST = 'events/CHANGE_BOOKMARK_REQUEST';
+export const CHANGE_BOOKMARK_REQUEST = 'entries/CHANGE_BOOKMARK_REQUEST';
 /** entries actiontype: change bookmarked state of entry */
-export const CHANGE_BOOKMARK_SUCCESS = 'events/CHANGE_BOOKMARK_SUCCESS';
+export const CHANGE_BOOKMARK_SUCCESS = 'entries/CHANGE_BOOKMARK_SUCCESS';
 /** entries actiontype: change bookmarked state of entry */
-export const CHANGE_BOOKMARK_FAILURE = 'events/CHANGE_BOOKMARK_FAILURE';
+export const CHANGE_BOOKMARK_FAILURE = 'entries/CHANGE_BOOKMARK_FAILURE';
 
 /** entries actiontype: change follow state of entry */
-export const CHANGE_FOLLOW_REQUEST = 'events/CHANGE_FOLLOW_REQUEST';
+export const CHANGE_FOLLOW_REQUEST = 'entries/CHANGE_FOLLOW_REQUEST';
 /** entries actiontype: change follow state of entry */
-export const CHANGE_FOLLOW_SUCCESS = 'events/CHANGE_FOLLOW_SUCCESS';
+export const CHANGE_FOLLOW_SUCCESS = 'entries/CHANGE_FOLLOW_SUCCESS';
 /** entries actiontype: change follow state of entry */
-export const CHANGE_FOLLOW_FAILURE = 'events/CHANGE_FOLLOW_FAILURE';
+export const CHANGE_FOLLOW_FAILURE = 'entries/CHANGE_FOLLOW_FAILURE';
 
 /** entries actiontype: change users vote for entry */
-export const CHANGE_VOTE_REQUEST = 'events/CHANGE_VOTE_REQUEST';
+export const CHANGE_VOTE_REQUEST = 'entries/CHANGE_VOTE_REQUEST';
 /** entries actiontype: change users vote for entry */
-export const CHANGE_VOTE_SUCCESS = 'events/CHANGE_VOTE_SUCCESS';
+export const CHANGE_VOTE_SUCCESS = 'entries/CHANGE_VOTE_SUCCESS';
 /** entries actiontype: change users vote for entry */
-export const CHANGE_VOTE_FAILURE = 'events/CHANGE_VOTE_FAILURE';
+export const CHANGE_VOTE_FAILURE = 'entries/CHANGE_VOTE_FAILURE';
 
 /** entries actiontype: delete entry */
-export const DELETE_ENTRY_REQUEST = 'events/DELETE_ENTRY_REQUEST';
+export const DELETE_ENTRY_REQUEST = 'entries/DELETE_ENTRY_REQUEST';
 /** entries actiontype: delete entry */
-export const DELETE_ENTRY_SUCCESS = 'events/DELETE_ENTRY_SUCCESS';
+export const DELETE_ENTRY_SUCCESS = 'entries/DELETE_ENTRY_SUCCESS';
 /** entries actiontype: delete entry */
-export const DELETE_ENTRY_FAILURE = 'events/DELETE_ENTRY_FAILURE';
+export const DELETE_ENTRY_FAILURE = 'entries/DELETE_ENTRY_FAILURE';
+
+/** entries actiontype: mark/log entry as read by user */
+export const READ_ENTRY_REQUEST = 'entries/READ_ENTRY_REQUEST';
+/** entries actiontype: mark/log entry as read by user */
+export const READ_ENTRY_SUCCESS = 'entries/READ_ENTRY_SUCCESS';
+/** entries actiontype: mark/log entry as read by user */
+export const READ_ENTRY_FAILURE = 'entries/READ_ENTRY_FAILURE';
 
 /** entries actiontype: load more entries into feed */
-export const LOAD_MORE_ENTRIES_REQUEST = 'events/LOAD_MORE_ENTRIES_REQUEST';
+export const LOAD_MORE_ENTRIES_REQUEST = 'entries/LOAD_MORE_ENTRIES_REQUEST';
 /** entries actiontype: load more entries into feed */
-export const LOAD_MORE_ENTRIES_SUCCESS = 'events/LOAD_MORE_ENTRIES_SUCCESS';
+export const LOAD_MORE_ENTRIES_SUCCESS = 'entries/LOAD_MORE_ENTRIES_SUCCESS';
 /** entries actiontype: load more entries into feed */
-export const LOAD_MORE_ENTRIES_FAILURE = 'events/LOAD_MORE_ENTRIES_FAILURE';
+export const LOAD_MORE_ENTRIES_FAILURE = 'entries/LOAD_MORE_ENTRIES_FAILURE';
 
 /** entries actiontype: post a new entry */
-export const POST_ENTRY_REQUEST = 'events/POST_ENTRY_REQUEST';
+export const POST_ENTRY_REQUEST = 'entries/POST_ENTRY_REQUEST';
 /** entries actiontype: post a new entry */
-export const POST_ENTRY_SUCCESS = 'events/POST_ENTRY_SUCCESS';
+export const POST_ENTRY_SUCCESS = 'entries/POST_ENTRY_SUCCESS';
 /** entries actiontype: post a new entry */
-export const POST_ENTRY_FAILURE = 'events/POST_ENTRY_FAILURE';
+export const POST_ENTRY_FAILURE = 'entries/POST_ENTRY_FAILURE';
 
 /** entries actiontype: subscribe to entries */
-export const SUBSCRIBE_ENTRIES_REQUEST = 'events/SUBSCRIBE_ENTRIES_REQUEST';
+export const SUBSCRIBE_ENTRIES_REQUEST = 'entries/SUBSCRIBE_ENTRIES_REQUEST';
 /** entries actiontype: subscribe to entries */
-export const SUBSCRIBE_ENTRIES_SUCCESS = 'events/SUBSCRIBE_ENTRIES_SUCCESS';
+export const SUBSCRIBE_ENTRIES_SUCCESS = 'entries/SUBSCRIBE_ENTRIES_SUCCESS';
 /** entries actiontype: subscribe to entries */
-export const SUBSCRIBE_ENTRIES_FAILURE = 'events/SUBSCRIBE_ENTRIES_FAILURE';
+export const SUBSCRIBE_ENTRIES_FAILURE = 'entries/SUBSCRIBE_ENTRIES_FAILURE';
 
 /** entries actiontype: subscribe to list of entries */
-export const SUBSCRIBE_ENTRY_LIST_REQUEST = 'events/SUBSCRIBE_ENTRY_LIST_REQUEST';
+export const SUBSCRIBE_ENTRY_LIST_REQUEST = 'entries/SUBSCRIBE_ENTRY_LIST_REQUEST';
 /** entries actiontype: subscribe to list of entries */
-export const SUBSCRIBE_ENTRY_LIST_SUCCESS = 'events/SUBSCRIBE_ENTRY_LIST_SUCCESS';
+export const SUBSCRIBE_ENTRY_LIST_SUCCESS = 'entries/SUBSCRIBE_ENTRY_LIST_SUCCESS';
 /** entries actiontype: subscribe to list of entries */
-export const SUBSCRIBE_ENTRY_LIST_FAILURE = 'events/SUBSCRIBE_ENTRY_LIST_FAILURE';
+export const SUBSCRIBE_ENTRY_LIST_FAILURE = 'entries/SUBSCRIBE_ENTRY_LIST_FAILURE';
 
 /** entries actiontype: unsubscribe from entries */
-export const UNSUBSCRIBE_ENTRIES_REQUEST = 'events/UNSUBSCRIBE_ENTRIES_REQUEST';
+export const UNSUBSCRIBE_ENTRIES_REQUEST = 'entries/UNSUBSCRIBE_ENTRIES_REQUEST';
 /** entries actiontype: unsubscribe from entries */
-export const UNSUBSCRIBE_ENTRIES_SUCCESS = 'events/UNSUBSCRIBE_ENTRIES_SUCCESS';
+export const UNSUBSCRIBE_ENTRIES_SUCCESS = 'entries/UNSUBSCRIBE_ENTRIES_SUCCESS';
 /** entries actiontype: unsubscribe from entries */
-export const UNSUBSCRIBE_ENTRIES_FAILURE = 'events/UNSUBSCRIBE_ENTRIES_FAILURE';
+export const UNSUBSCRIBE_ENTRIES_FAILURE = 'entries/UNSUBSCRIBE_ENTRIES_FAILURE';
 
 /** entries actiontype: unsubscribe from list of entries */
-export const UNSUBSCRIBE_ENTRY_LIST_REQUEST = 'events/UNSUBSCRIBE_ENTRY_LIST_REQUEST';
+export const UNSUBSCRIBE_ENTRY_LIST_REQUEST = 'entries/UNSUBSCRIBE_ENTRY_LIST_REQUEST';
 /** entries actiontype: unsubscribe from list of entries */
-export const UNSUBSCRIBE_ENTRY_LIST_SUCCESS = 'events/UNSUBSCRIBE_ENTRY_LIST_SUCCESS';
+export const UNSUBSCRIBE_ENTRY_LIST_SUCCESS = 'entries/UNSUBSCRIBE_ENTRY_LIST_SUCCESS';
 /** entries actiontype: unsubscribe from list of entries */
-export const UNSUBSCRIBE_ENTRY_LIST_FAILURE = 'events/UNSUBSCRIBE_ENTRY_LIST_FAILURE';
+export const UNSUBSCRIBE_ENTRY_LIST_FAILURE = 'entries/UNSUBSCRIBE_ENTRY_LIST_FAILURE';
 
 /** entries actiontype: update of entry-data */
 export const UPDATE_ENTRIES = "entries/UPDATE_ENTRIES";
