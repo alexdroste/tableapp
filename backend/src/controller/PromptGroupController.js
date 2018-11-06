e9782fff04775e694802972e92db132a1a362735
Alexander Droste <alexander.droste@progmem.de>
2018-11-06 02:02:06 +0100

    backend: PrompGroupController swap groups after expiry

--- backend/src/controller/PromptGroupController.js
+++ backend/src/controller/PromptGroupController.js
@@ -45,26 +45,33 @@ class PromptGroupController {
     }
 
 
-    async _joinGroup(userId, eventId) {
-        let group = 0;
-        let groupCount = { 0: 0, 1: 0 };
-
+    async _getNextGroup() {
         const groupRatio = await this._db.collection('promptgroup').aggregate([
-            { $match: { eventId } },
+            { $match: { 
+                eventId,
+                toDate:  { $gt: new Date() },
+            } },
             { $group: {
                 _id: '$group',
                 count: { $sum: 1 },
             } }
         ]).toArray();
 
+        const groupCount = { 0: 0, 1: 0 };
         groupRatio.forEach(d => {
             groupCount[d._id] = d.count;
         });
 
         if (Math.abs(groupCount[0] - groupCount[1]) < maxUnbalance)
-            group = Math.floor(Math.random() * 2);
+            return Math.floor(Math.random() * 2);
         else
-            group = groupCount[0] < groupCount[1] ? 0 : 1;
+            return groupCount[0] < groupCount[1] ? 0 : 1;
+    }
+
+
+    async _joinGroup(userId, eventId, group) {
+        if (typeof group !== 'number')
+            group = await this._getNextGroup();
 
         const res = await this._db.collection('promptgroup').insertOne({
             eventId,
@@ -89,22 +96,24 @@ class PromptGroupController {
 
     async getGroup(userId, eventId) {
         /*
-        lese gruppe für nutzer aus db (prop: to > aktuelles datum)
+        lese letzte gruppe für nutzer aus db 
         wenn kein eintrag vorhanden:
-            lese verhältnis gruppenmitglieder aus db
+            lese verhältnis gruppenmitglieder aus db (prop: to > aktuelles datum)
             wenn verhältnis zwischen gruppen ausgeglichen (abweichung < treshold):
                 random gruppe bestimmen
             sonst:
                 gruppe mit weniger mitgliedern wählen
             gruppe für nutzer in db speichern (mit props from - to)
+        sonst wenn letzte gruppe abgelaufen (prop: to <= aktuelles datum):
+            gruppe tauschen
         return gruppe
         */
-        const doc = await this._db.collection('promptgroup').findOne({ 
-            eventId, userId,
-            toDate:  { $gt: new Date() }
-        });
+        const doc = await this._db.collection('promptgroup').findOne(
+            { eventId, userId }, { sort: { toDate: -1 } });
         if (!doc)
             return await this._joinGroup(userId, eventId);
+        if (doc.toDate.getTime() <= Date.now())
+            return await this._joinGroup(userId, eventId, doc.group === 0 ? 1 : 0);
         return doc.group;
     }
 }
